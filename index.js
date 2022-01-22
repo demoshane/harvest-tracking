013b05b161f503744c2980db4ac4d26390c8aba3
Timo Kirkkala <timo.kirkkala@wunder.io>
2022-01-22 22:09:09 +0200

    Make it beautiful with colors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const harvest = require('./lib/harvest');
 const argv = require('minimist')(process.argv.slice(2));
 const validator = require('validator');
+const colors = require('colors');
 // Use moment for weekday calculations. TODO: Probably need to replace this for something better later on.
 moment = require('moment');
 require('moment-weekday-calc');
@@ -14,6 +15,10 @@ const unpaidHours = parseInt(process.env.HARVEST_UNPAID ? process.env.HARVEST_UN
 const overtime_hours_from_last_year = parseInt(process.env.HARVEST_OVERTIME_LAST_YEAR ? process.env.HARVEST_OVERTIME_LAST_YEAR : 0);
 const paid_overtime_hours = parseInt(process.env.HARVEST_PAID_OVERTIME ? process.env.HARVEST_PAID_OVERTIME : 0);
 const dayLength = parseFloat(process.env.HARVEST_DAY_LENGTH ? process.env.HARVEST_DAY_LENGTH : 7.5);
+
+console.log('\n*************************'.brightMagenta);
+console.log('* HARVEST HOURS BALANCE *'.brightMagenta.bold);
+console.log('*************************\n'.brightMagenta);
 
 // Check for from/to dates if user passed arguments.
 if (Object.keys(argv).length > 1) {
@@ -34,58 +39,52 @@ async function main() {
       4,
       5
     ]);
-    console.log('(calculating worktime balance...)');
-    console.log('TIMEFRAME'+'\n'+`Period: ${date_from} - ${date_to}`);
-    console.log('\n'+'STATISTICS')
+
+    console.log('\nTIMEFRAME'.bold);
+    console.log(`${date_from} - ${date_to}`.green);
+    console.log('\nSTATISTICS'.bold)
     // Output how many weekdays on period.
-    console.log('Weekdays (Mon-Fri) so far for given period: ' + totalWorkdays);
+    console.log('Working days (Mon-Fri) for given period: ' + `${totalWorkdays}`.bold);
 
     const shouldBe = totalWorkdays * dayLength;
     // Show many hours there should be during the period. PS: Pay attention that if you balanced hours from previous year, this is off.
     // Thus it's recommended to update balance from last year as negative or positive to "overtime hours from last year" above.
-    console.log('Hours needed: ' + shouldBe);
+    console.log('Hours required: ' + `${shouldBe}`.bold);
     // Let's take into account balance. This show the actual hours needed taken balance into account.
     console.log(
-      'Hours needed, period start balance taken into account: ' +
-        (shouldBe - overtime_hours_from_last_year)
+      'Hours required, period start balance taken into account: ' +
+        `${(shouldBe - overtime_hours_from_last_year)}`.bold
     );
 
     // Get user entries from Harvest for the period.
     const entries = await harvest.getUserEntries(user_id, date_from, date_to);
     // Calculate how many hours has been done.
-    const total = entries.reduce((sum, dayEntry) => sum + dayEntry.hours, 0);
     // Set total.
-    const total_adjusted = total;
+    const total_adjusted = entries.reduce((sum, dayEntry) => sum + dayEntry.hours, 0);
     // Show many hours there has been marked to Harvest.
-    console.log('Total hours marked in Harvest: ' + total_adjusted);
+    console.log('Total hours marked in Harvest: ' + `${total_adjusted}`.bold);
     // Show how many hours has been marked as balance from last year.
-    console.log(
-      `Last year under- or overtime hours transferred to quota: ${overtime_hours_from_last_year}`
-    );
+    console.log('Last year under- or overtime hours transferred to quota: ' + `${overtime_hours_from_last_year}`.bold);
     // Has there been any paid overtime? If so show this, but this is deducted from overtime as it's paid.
-    console.log(
-      `Paid overtime hours: ${paid_overtime_hours}`
-    );
+    console.log('Paid overtime hours: ' + `${paid_overtime_hours}`.bold);
     // Has there been any unpaid overtime? Show this but this should be added to balance as it's unpaid and agreed balance.
-    console.log(
-      `Unpaid absence hours: ${unpaidHours}`
-    );
+    console.log('Unpaid absence hours: ' + `${unpaidHours}`.bold);
 
-    // Calculate balance.
-    const diff =
-      total_adjusted - shouldBe - paid_overtime_hours + overtime_hours_from_last_year + unpaidHours;
-    const diffWithoutBalance =
-      diff - overtime_hours_from_last_year;
+    // Calculate balances.
+    const diff = total_adjusted - shouldBe - paid_overtime_hours + overtime_hours_from_last_year + unpaidHours;
+    const diffWithoutBalance = diff - overtime_hours_from_last_year;
+
     // Show balance in hrs.
-    console.log('\n'+'TOTAL BALANCE');
-    console.log('Diff (h): ' + diff.toFixed(2));
+    console.log('\nTOTAL BALANCE'.bold);
+    console.log('Diff (h): ' + `${diff.toFixed(2)}`.bold);
     // Show balance in days.
-    console.log('Diff (days): ' + (diff.toFixed(2)/dayLength));
+    console.log('Diff (days): ' + `${(diff.toFixed(2)/dayLength)}`.bold);
     // Show balance without previous year(s).
-    console.log('\n'+'TOTAL BALANCE FOR CURRENT YEAR ONLY (No previous balances)');
-    console.log('Diff (h): ' + diffWithoutBalance.toFixed(2));
+    console.log('\nTOTAL BALANCE FOR CURRENT YEAR ONLY'.bold);
+    console.log('(No previous balances)');
+    console.log('Diff (h): ' + `${diffWithoutBalance.toFixed(2)}`.bold);
     // Show balance in days.
-    console.log('Diff (days): ' + (diffWithoutBalance.toFixed(2)/dayLength));
+    console.log('Diff (days): ' + `${diffWithoutBalance.toFixed(2)/dayLength}`.bold);
   } catch (e) {
     console.error(e);
   }
@@ -99,7 +98,7 @@ function getValidDate(dateStr) {
     return dateStr;
   }
   else {
-    console.log('Invalid date input "' + dateStr + '"');
+    console.log(`\nERROR: Invalid date input '${dateStr}'\n\n`.red);
     return false
   }
 }
